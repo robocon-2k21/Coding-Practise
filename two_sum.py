6e8222a9a5e3112a8c2f2e820b65c12c873d0cee
vedant-beep <66842455+vedant-beep@users.noreply.github.com>
2021-03-21 22:12:55 +0530

    Add files via upload

---
+++ two_sum.py
@@ -0,0 +1,18 @@
+def two_sum(given_array, target_sum):
+    given_array.sort()
+    i = 0
+    j = len(given_array) - 1
+    while i <= j:
+        if given_array[i] + given_array[j] == target_sum:
+            return 1
+        elif given_array[i] + given_array[j] < target_sum:
+            i += 1
+        # elif given_array[i] + given_array[j] > target_sum:
+        #     j -= 1
+        else:
+            j -= 1
+    return 0
+
+
+arr1 = [-2, 1, 2, 4, 7, 11]
+print(two_sum_using_binary(arr1, 13))
