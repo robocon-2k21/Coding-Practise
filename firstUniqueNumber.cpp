8e9646491b6c5acae96f831d7ad15aacc4ade863
phadkesharan <phadkesharan@gmail.com>
2021-03-24 15:45:41 +0530

    Create firstUniqueNumber.cpp

---
+++ firstUniqueNumber.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int n = s.length();
+        unordered_map <char, int> m;
+        for(int i=0;i<n;i++){
+            m[s[i]]++;
+        }
+        
+        for(int i=0;i<n;i++){
+            if(m[s[i]] == 1)
+                return i;
+        }
+        return -1;
+    }
+};
